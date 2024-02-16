ffb2db9c2602c75018fc1f58d7c385a8d649b184
shreyu2000 <shreyamdake2000@gmail.com>
2024-02-16 15:38:25 +0530

    apis

---
+++ src/apis/job.js
@@ -0,0 +1,81 @@
+import axios from "axios";
+
+const backendUrl = import.meta.env.BACKEND_URL;
+
+export const getJobsByFilter = async ({title, skills})=> {
+    const params = {
+        title,
+    };
+
+    if(skills.length) params.skills = skills.toString();
+    
+    try {
+        const response = await axios.get(`${backendUrl}/jobs`, {
+            params: {
+                ...params,
+            }
+        });
+
+        const {jobs} = response.data.data;
+        return {data: jobs, error: ""}
+
+    } catch (error) {
+        return {data: null, error: "Something went wrong!"}
+    }
+}
+
+
+export const getJobPost = async (jobId)=> {
+
+    try {
+        const response = await axios.get(`${backendUrl}/jobs/${jobId}`);
+
+        const {job} = response.data.data;
+        return {data: job, error: ""}
+
+    } catch (error) {
+        return {data: null, error: error?.response?.data?.message || "Something went wrong!"}
+    }
+}
+
+export const createJobPost = async (jobData)=> {
+    const accessToken = localStorage.getItem("accessToken");
+
+    try {
+        const response = await axios.post(`${backendUrl}/jobs`, 
+        {
+            ...jobData
+        }, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        });
+
+        const {job} = response.data.data;
+        return {data: job, error: ""}
+
+    } catch (error) {
+        return {data: null, error: "Something went wrong!"}
+    }
+}
+
+export const updateJobPost = async (jobData, jobId)=> {
+    const accessToken = localStorage.getItem("accessToken");
+
+    try {
+        const response = await axios.put(`${backendUrl}/jobs/${jobId}`, 
+        {
+            ...jobData
+        }, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        });
+
+        const {job} = response.data.data;
+        return {data: job, error: ""}
+
+    } catch (error) {
+        return {data: null, error: "Something went wrong!"}
+    }
+}
